3bb2ac1ae97233a154937e10c2b447d7d627d751
snidarian <cephalopod31956@gmail.com>
2022-03-11 16:26:33 -0500

    aflskdf

---
+++ bash_builtin_variables.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+
+# $0
+echo "The name of the current shell:"
+echo "$0"
+
+# $?
+echo "Exit code of previous command:"
+echo "$?"
+
+# $_
+echo "The previous command run in this shell:"
+echo "$_"
+
+# $$
+echo ""
+echo "$$"
+
+
+# $#
+echo "The command line arguments of a command"
+echo "$#"
